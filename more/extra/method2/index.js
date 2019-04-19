fb37a5cd77a686e7acab8bd2930c6ec8d5fbfe37
Soliman <solieman.elsaber@gmail.com>
2019-04-19 15:18:36 +0800

    Fixing directory path

---
+++ more/extra/method2/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const Adapter = require('./adapter');
+
+class SmallAdapter extends Adapter {
+    constructor(req){
+        super(req);
+        this.formatted_data = this.format_req_body(req);
+    }
+
+    events(req, res) {
+        const data = this.formatted_data;
+        const app_name = data.app_name;
+
+        res.status(405).send("Method Not Allowed");
+    }
+
+    identify(req, res) {
+        const data = this.formatted_data;
+        const app_name = data.app_name;
+
+        res.status(405).send("Method Not Allowed");
+    }
+}
+
+let braze = null;
+
+app.post('/event', function (req, res) {
+    braze = new SmallAdapter(req);
+    braze.events(req, res);
+});
+app.post('/identify',function (req, res) {
+    braze = new SmallAdapter(req);
+    braze.identify(req, res);
+});
+exports.small = app;
